2a0e003c4d65a49763c36964e7a633a0a407f506
Jamorno <100228770+Jamorno@users.noreply.github.com>
2023-05-05 20:06:57 +0700

    Update Footer.js

--- components/Footer.js
+++ components/Footer.js
@@ -2,7 +2,7 @@ import Image from "next/image"
 
 export default function Footer() {
     return (
-        <footer className="lg:mb-20">
+        <footer>
             <div className="flex space-x-4 items-center justify-center pt-10 pb-16 lg:hidden">
                 <Image className="cursor-pointer" src="/client-databiz.svg" width="80" height="10" alt="client-databiz-image" />
                 <Image className="cursor-pointer" src="/client-audiophile.svg" width="50" height="10" alt="client-audiophile-image" />
@@ -11,7 +11,7 @@ export default function Footer() {
             </div>
 
             {/* Footer desktop */}
-            <div className="space-x-8 items-center justify-center hidden lg:flex lg:absolute bottom-2 left-40">
+            <div className="space-x-8 items-center justify-center hidden lg:flex lg:absolute bottom-32 left-40">
                 <Image className="cursor-pointer" src="/client-databiz.svg" width="100" height="10" alt="client-databiz-image" />
                 <Image className="cursor-pointer" src="/client-audiophile.svg" width="70" height="10" alt="client-audiophile-image" />
                 <Image className="cursor-pointer" src="/client-meet.svg" width="100" height="10" alt="client-meet-image" />
